a55723125180bfb5ab90a62a6509e77c23fd7590
Yaroslav <fil.i.al4ik@gmail.com>
2024-09-26 19:39:14 +0200

    24.09hw

--- src/script.js
+++ src/script.js
@@ -28,6 +28,51 @@ app.get("/post", (req, res) => {
     res.render("post", context)
 })
 
+const posts = [
+    {
+        id: 1,
+        name: 'post1',
+        time: moment().format("YYYY/MM/DD hh:mm:ss"),
+        description: 'description',
+        author:"Author1"
+    },
+    {
+        id: 2,
+        name: 'post2',
+        time: moment().format("YYYY/MM/DD hh:mm:ss"),
+        description: 'description2',
+        author:"Author2"
+    },
+    {
+        id: 3,
+        name: 'post3',
+        time: moment().format("YYYY/MM/DD hh:mm:ss"),
+        description: 'description3',
+        author:"Author3"
+    },
+]
+app.get('/posts/:id', (req, res) => {
+    const id = req.params.id
+    if (id > 0 && id <= posts.length) {
+        const context = { posts: posts[id - 1], error: null }   //без этого null у меня выводилась ошибка но не выводились данные нормальных id жаловалось что не может найти этот тип данных
+        res.render('posts', context)
+    } else {
+    res.render('posts', { error: "Такого поста не существует" })} // а так как я и говорил просто проверка списка по длинне и вывод ошибки через контекст
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT,HOST,()=>{
     console.log(`Server is running on http://${HOST}:${PORT}`)
 })
